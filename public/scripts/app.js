43824f7079f9b15dc4904b0519249360e63ceef9
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-30 23:47:16 +0000

    fix button and formatting for createpolls page, ready to take in database input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,10 @@ $(document).ready(()=>{
 
 
   $(function() {
-    const $button = $('#addOption');
+    const $button = $('.addOption');
     //catches user input
     $button.on('submit', (event) => {
+      console.log("umm")
       event.preventDefault();
         let $option = $( `<div><textarea class="optionBox" name="text" placeholder="Enter option"></textarea></div>`)
         $('.optionBox').append($option);
