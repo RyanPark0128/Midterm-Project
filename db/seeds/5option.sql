a900221b3c1e22a0c6c4bd458e5b2efa8d215952
jswelsh <james.sinclair.welsh@gmail.com>
2019-07-31 21:32:39 +0000

    create seeds for options table and refactor / reduce querry length using RETURN chained to a INSERT,  instead of a seperate SELECT query

---
+++ db/seeds/5option.sql
@@ -0,0 +1,15 @@
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (1, 'go for bike ride', '6776676778686766766776', 2);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (1, 'go for a hike', '456456546546464565465465465464564564544564', 1);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (1, 'go to movies', '12113212312312321312312312321312312321321', 3);
+
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (2, 'sleep over party #noBoysAllowed', '111111111111111', 4);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (2, 'LAN party original halo', '222222222222222222222', 2);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (2, 'mario party', '3333333333333333333333333333333333333333333333', 3);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (2, 'no pants party', '4444444444444444444444444444444444', 1);
+
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'go to space', 'kkkkkkkkkkkkkkkkkkkkkkkk', 5);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'go to africa', 'jjjjjjjjjjjjjjjjjjj', 1);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'go to the zoo', 'iiiiiiiiiiiiiiiiiii', 3);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'go for a walk', 'hhhhhhhhhhhhhhhhhhhhhhh', 2);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'go go gadget', 'gggggggggggggggggggggggg', 4);
+INSERT INTO options (survey_id, choice, description, total_rank) VALUES (3, 'do the things', 'ffffffffffffffffffffffff', 6);
--- routes/createPoll.js
+++ routes/createPoll.js
@@ -17,49 +17,41 @@ router.get("/createPoll", requiresLogin, (req, res) => {
 router.post("/createPoll", requiresLogin, (req, response) => {
   const adminCode = generateRandomString();
   const respondentCode = generateRandomString();
-  //Parameterized query needs to be added
   // need to sequence promises
   console.log(req.session.userId, "1---2")
   let adminId;
   let surveyId;
+                                    //Parameterized query needs to be added
     db.query(`SELECT id FROM admins WHERE email = '${req.session.userId}';`)
     .then(res => {
-    let hold = res.rows;
-    console.log(hold[0]['id'],'1---3');
-    adminId = hold[0]['id'];
-    return db.query(`INSERT INTO surveys (admin_id, admin_code, respondent_code, title) VALUES (${adminId}, '${adminCode}', '${respondentCode}', '${req.body['title']}') RETURNING "id"`)
-  })
-    //Parameterized query needs to be added, can be shorten using above with returning??!?!? i think
-/*   let surveyId = db.query(`SELECT SCOPE_IDENTITY()`)
-  .then(data => {
-    res.json({data});
-  })
-  .catch(err => {
-    res
-      .status(500)
-      .json({ error: err.message });
-  }); */
-  .then(res => {
-    console.log(res, "2--1")
-    surveyId = res.rows[0]['id'];
-    for (let i = 0; i < req.body['option'].length; i++) {
-      console.log('1---4');
-      db.query(`INSERT INTO options (survey_id, choice, description, total_rank) VALUES (${surveyId}, '${req.body['option'][i]}', '${req.body['description'][i]}', 0)`)
-    .then(() => {
-      console.log('1---5');
+      let hold = res.rows;
+      console.log(hold[0]['id'],'1---3');
+      adminId = hold[0]['id'];
+      return db.query(`INSERT INTO surveys (admin_id, admin_code, respondent_code, title) VALUES (${adminId}, '${adminCode}', '${respondentCode}', '${req.body['title']}') RETURNING "id"`)
+    })
+    .then(res => {
+      console.log(res, "2--1")
+      surveyId = res.rows[0]['id'];
+      for (let i = 0; i < req.body['option'].length; i++) {
+        console.log('1---4');
+                                                      //Parameterized query needs to be added
+        db.query(`INSERT INTO options (survey_id, choice, description, total_rank) VALUES (${surveyId}, '${req.body['option'][i]}', '${req.body['description'][i]}', 0)`)
+        .then(() => {
+          console.log('1---5');
+          return
+        })
+      }
     })
     .catch(err => {
       response
-        .status(500)
-        .json({ error: err.message});
-  })
-}
-  });
-
+      .status(500)
+      .json({ error: err.message});
+    })
+  response.redirect('/vote_result');
 });
-  //Parameterized query needs to be added
+
 module.exports = router;
-//tracks tweets and updates them
+
 
 
 
