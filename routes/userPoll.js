3295792002af7fbfcb0d6945aa186e3126544093
jswelsh <james.sinclair.welsh@gmail.com>
2019-08-01 06:06:40 +0000

    refactor, clean code up and work on userpoll not passing title

--- routes/userPoll.js
+++ routes/userPoll.js
@@ -5,23 +5,22 @@ const dbParams = require('../lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
-router.get("/userPoll/:id", (req, response) => {
-  
-  db.query(`
-    SELECT * FROM surveys WHERE respondent_code = $1
-    `,[req.params.id])
+router.get("/userPoll/:id", (req, res) => {
+  db.query(`SELECT title FROM surveys WHERE respondent_code = '${req.params.id}'`)
     .then(res => {
-      const title = res.rows[0].title
-      response.render("userPoll", {title});
+      litle = res.rows[0]
+
+      console.log(title, "1")
+      response.render("userPoll", title['title']);
     }
   )
   })
 
-router.post("/userPoll/:id", (req, res) => {
+router.post("/userPoll", (req, res) => {
     res.redirect("/vote_result")
   });
 
-//Kira@google.com
+//Kira@google.com    http://localhost:8080/userpoll/55555
 //   1 |        1 | 12345      | 55555           | dinner
 //   2 |        2 | 23456      | 44444           | saturday
 //   3 |        3 | 45678      | 88888           | party
