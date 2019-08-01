20bbc4f2069bb301993db318c2bf7e5955264858
jswelsh <james.sinclair.welsh@gmail.com>
2019-08-01 15:57:47 +0000

    refactor: clean up querries for create polls and login, add commenting and make formatting more consistent, change varaible names to be more descriptive and less ambigious given abstraction from original files they are now being called from.

--- query/createPollQueries.js
+++ query/createPollQueries.js
@@ -3,35 +3,43 @@ const dbParams = require('../lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
+//generates random strings to be used for short url
+//to be distibuted to users or admins of surveys
 const generateRandomString = require('../helper');
 const adminCode = generateRandomString();
 const respondentCode = generateRandomString();
 
-
-
 function getAdminIdWithCookie (req){
+  const adminId = req.session.userId;
+  const title = req.body['title'];
   const updateSurveyToDb =
-`INSERT INTO surveys (admin_id, admin_code, respondent_code, title)
- VALUES (${req.session.userId}, '${adminCode}', '${respondentCode}', '${req.body['title']}')
- RETURNING "id"`;
+        `INSERT INTO surveys (admin_id, admin_code, respondent_code, title)
+         VALUES (${adminId}, '${adminCode}', '${respondentCode}', '${title}')
+         RETURNING "id"`;
 
-
+  //nested query to be executed
+  //adminId is provided so that
+  //survey can be associated w/
   db.query(updateSurveyToDb)
     .then(res => {
       const surveyId = res.rows[0]['id'];
-      console.log(surveyId, "survey id got set???? createpolqueries")
-      const  updateOptionsToDb =
-      `INSERT INTO options (survey_id, choice, description, total_rank)
-       VALUES (${surveyId}, '${req.body['option'][i]}', '${req.body['description'][i]}', 0)`
+      const choice = req.body['option'][i];
+      const description = req.body['description'][i];
+      const totalRank = 0
+      const updateOptionsToDb =
+            `INSERT INTO options (survey_id, choice, description, total_rank)
+             VALUES (${surveyId}, '${choice}', '${description}', ${totalRank})`;
+      //looping through each option passed
+      //in when n options is, n > 0.
       for (let i = 0; i < req.body['option'].length; i++) {
         db.query(updateOptionsToDb)
-        .then(() => {
-          return
-        })
-      }
+          .then(() => {
+            return
+          })
+        }
     })
     .catch(err => {
-      response
+      res
       .status(500)
       .json({ error: err.message});
     })
--- routes/createPoll.js
+++ routes/createPoll.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const {requiresLogin} = require('../lib/middleware/authentication')
 const {getAdminIdWithCookie} = require('../query/createPollQueries')
 
-//requiresLogin check if user is logged in
+                        //requiresLogin check if user is logged in
 router.get("/createPoll", requiresLogin, (req, response) => {
-  console.log("req.session.userId):",req.session.userId, "cookie session set, directed to render createpoll")
   response.render("createPoll")
 });
-
+                         //requiresLogin check if user is logged in
 router.post("/createPoll", requiresLogin, (req, response) => {
   getAdminIdWithCookie(req)
   response.redirect('/vote_result');
