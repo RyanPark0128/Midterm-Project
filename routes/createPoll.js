cb1e24b1f68f5adfd2efb39a5aeae8393eb8dd1e
Support <vazywon@hotmail.com>
2019-08-01 22:38:54 +0000

    added scroll top, email, redirect

--- routes/createPoll.js
+++ routes/createPoll.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {requiresLogin} = require('../lib/middleware/authentication')
 const {getAdminIdWithCookie} = require('../query/createPollQueries')
+const nodemailer = require('nodemailer');
 
                         //requiresLogin check if user is logged in
 router.get("/createPoll", requiresLogin, (req, response) => {
@@ -10,7 +11,29 @@ router.get("/createPoll", requiresLogin, (req, response) => {
                          //requiresLogin check if user is logged in
 router.post("/createPoll", requiresLogin, (req, response) => {
   getAdminIdWithCookie(req)
-  response.redirect('/vote_result');
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'iwantservicer@gmail.com',
+      pass: 'Aa1Bb2Cc3'
+    }
+  });
+
+  const mailOptions = {
+    from: 'iwantservicer@gmail.com',
+    to: 'vazywon@hotmail.com',
+    subject: 'Here is the link for the poll!',
+    text: 'admin link: `www.google.ca'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+    response.redirect('/vote_result');
 });
 
 module.exports = router;
