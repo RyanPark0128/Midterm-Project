bddf470b8a9170f9ee584539f7bcfdb131df087f
jswelsh <james.sinclair.welsh@gmail.com>
2019-08-01 23:14:26 +0000

    finish query to pull survey results and connect to routing with refactor

--- query/resultsQuerries.js
+++ query/resultsQuerries.js
@@ -4,11 +4,27 @@ const db = new Pool(dbParams);
 db.connect();
 
 
-function getSurveyWithAdminCode (){
+function getSurveyWithAdminCode (adminCode){
+  const surveyCode = adminCode;
+  const resultsWithAdminCode =
   `SELECT title, total_rank, admin_code, choice, description
    FROM options
    JOIN surveys
    ON options.survey_id = surveys.id
-   WHERE surveys.id = 1;`
+   WHERE surveys.id = ${surveyCode};`;
 
+  db.query(resultsWithAdminCode)
+   .then(res => {
+    res.rows.forEach(option => {
+      console.log(`${option.choice} has an description of ${option.description} and has a rank of ${option.total_rank}`);
+      return res;
+    })
+  })
+  .catch(err => {
+    res
+    .status(500)
+    .json({ error: err.message});
+  })
 }
+
+module.exports = {getSurveyWithAdminCode};
